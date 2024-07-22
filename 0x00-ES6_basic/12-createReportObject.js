3b18894d3d0dfb0177fdba3f1f5b9f66af507c06
rekebuba <abukiabdullahi1@gmail.com>
2024-07-23 00:37:44 +0300

    create Report Object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(dictionary) {
+      return Object.keys(dictionary).length;;
+    },
+  }
+}
