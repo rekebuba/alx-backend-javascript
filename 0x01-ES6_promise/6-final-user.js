5b1ddcc7c25956d4126efcb97d8e9d01f7f5bb68
rekebuba <abukiabdullahi1@gmail.com>
2024-07-24 22:26:38 +0300

    handle Profile Signup

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => {
+    .then((result) => (
       result.map((x) => ({
         status: x.status,
         value: x.status === 'fulfilled' ? x.value : String(x.reason),
-      }));
-    });
+      }))
+    ));
 }
