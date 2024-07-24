ac07ce666239dd33c65a76e621f3e62022d07b89
rekebuba <abukiabdullahi1@gmail.com>
2024-07-24 15:52:16 +0300

    0x01-ES6_promise

--- 0x01-ES6_promise/jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-    testEnvironment: 'node',
-    moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
-    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
-  };
