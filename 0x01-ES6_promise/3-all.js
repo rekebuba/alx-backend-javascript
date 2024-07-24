cc83f897d25cc82c260a4431be6e1bcf471fa47b
rekebuba <abukiabdullahi1@gmail.com>
2024-07-24 21:34:42 +0300

    handle Profile Signup

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from './utils'
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup(){
+export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-  .then((values) => {
-    console.log(values[0].body, values[1].firstName,values[1].lastName);
-  })
-  .catch(() => {
-    console.log('Signup system offline');
-  });
+    .then((values) => {
+      console.log(values[0].body, values[1].firstName, values[1].lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
