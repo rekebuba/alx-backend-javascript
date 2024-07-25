cefd21935393b3a16f0e06777d99c3440d796408
rekebuba <abukiabdullahi1@gmail.com>
2024-07-26 00:01:24 +0300

    Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (new.target !== Building && this.evacuationWarningMessage === undefined) {
-      throw new Error("Abstract method 'evacuationWarningMessage' must be implemented by subclass");
+      throw new Error("Class extending Building must override evacuationWarningMessage");
     }
     this.sqft = sqft;
   }
