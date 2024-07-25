da487adf83691478909f50e48f477f0a4ace4279
rekebuba <abukiabdullahi1@gmail.com>
2024-07-26 01:57:55 +0300

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,14 +1,13 @@
 import Car from './10-car';
 
-export default class EVCar {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+    super(brand, motor, color);
     this._range = range;
   }
 
   cloneCar() {
-    return new Car();
+    const Species = super.constructor[Symbol.species];
+    return new Species();
   }
 }
