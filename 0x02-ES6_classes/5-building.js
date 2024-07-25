6108c087df57e744982074ddaa9bb28efa486911
rekebuba <abukiabdullahi1@gmail.com>
2024-07-26 00:00:03 +0300

    Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error("Abstract method 'evacuationWarningMessage' must be implemented by subclass");
+    }
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = value;
+  }
+}
