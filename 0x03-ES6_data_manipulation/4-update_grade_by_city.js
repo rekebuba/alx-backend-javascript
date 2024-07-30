128a3d8c266ae7da5e9e85c005d9a97a64cc6bc0
rekebuba <abukiabdullahi1@gmail.com>
2024-07-31 00:31:03 +0300

    update Student Grade By City

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,3 @@
-import getStudentsByLocation from './2-get_students_by_loc';
-
 export default function updateStudentGradeByCity(getList, city, newGrades) {
   if (!Array.isArray(getList)) {
     return [];
@@ -7,13 +5,14 @@ export default function updateStudentGradeByCity(getList, city, newGrades) {
 
   const gradeMap = new Map(newGrades.map(({ studentId, grade }) => [studentId, grade]));
 
-  return getStudentsByLocation(getList, city).map((student) => {
-    if (gradeMap.has(student.id)) {
-      student.grade = gradeMap.get(student.id);
-    } else {
-      student.grade = 'N/A';
-    }
+  return getList.filter((value) => value.location === city)
+    .map((student) => {
+      if (gradeMap.has(student.id)) {
+        student.grade = gradeMap.get(student.id);
+      } else {
+        student.grade = 'N/A';
+      }
 
-    return student;
-  });
+      return student;
+    });
 }
