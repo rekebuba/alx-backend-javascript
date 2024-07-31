265871a7163afa1b85c2574e47a6809c0269fc0b
rekebuba <abukiabdullahi1@gmail.com>
2024-08-01 01:02:20 +0300

    0x04-TypeScript

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,2 +1,7 @@
-import { RowID } from './interface';
-import { RowElement } from './interface';
+import { RowID, RowElement } from './interface';
+
+export function insertRow(row: RowElement): number;
+
+export function deleteRow(rowId: RowID): void;
+
+export function updateRow(rowId: RowID, row: RowElement): number;
